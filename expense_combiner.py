1bd109a1497d9ddfd8f6451253947a61e8dff28d
AshleyLTW <missashleywong@gmail.com>
2018-07-11 16:13:35 +1000

    it works

--- expense_combiner.py
+++ expense_combiner.py
@@ -1,25 +1,41 @@
-import csv, sys, sqlite3
+import csv, sys, sqlite3, re
 
 # python3 [script name] [downloaded file]
 
 connection = sqlite3.connect("foo.db")
 cursor = connection.cursor()
 
+### Create table
 create = "CREATE TABLE Expenses(item text not null, date text not null, amount float not null);"
 cursor.execute(create)
 
+### Convert csv to db
 downloadedFile = sys.argv[1]
 with open(downloadedFile) as myfile:
 	readCSV = csv.DictReader(myfile)
 	for row in readCSV:
 		item = row['Category']
+		# Remove the time stamp on dates
 		date = row['Date']
+		sucDate = re.search(r"\d\d\d\d/\d\d/\d\d", date).group(0)
+
+		# Setting amount
 		amount = row['Value']
+
+		# Remove the negative sign 
 		if amount[0] == "-":
 			amount = amount[1:]
 
-		insert = "INSERT INTO expenses VALUES('" + item + "', '" + date + "', " + amount + ");"
+		insert = "INSERT INTO expenses VALUES('" + item + "', '" + sucDate + "', " + amount + ");"
 		cursor.execute(insert) 
+
+### Begin writing to new CSV file
+with open('output.csv', 'a', newline='') as output:
+	writer = csv.writer(output, delimiter=',')
+	writer.writerow(['Item', 'Date', 'Amount'])
+	### Combine purchases with the same category + date and write it
+	for row in cursor.execute("SELECT item, date, sum(amount) FROM expenses GROUP BY date, item ORDER BY date ASC;"):
+		writer.writerow(row)
 
 cursor.close()
 connection.commit()	
